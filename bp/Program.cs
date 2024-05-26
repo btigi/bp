6fec6e9017554bcafb008ba622aaf50a4ca58b86
btigi <btigi@users.noreply.github.com>
2024-05-26 16:00:59 +0100

    Add setting to determine where to save mp3s

--- bp/Program.cs
+++ bp/Program.cs
@@ -4,7 +4,7 @@ using NetCoreAudio;
 using OllamaSharp;
 
 var builder = new ConfigurationBuilder()
-                .AddJsonFile($"appsettings.json", true, true);
+                .AddJsonFile($"bp.json", true, true);
 
 var config = builder.Build();
 
@@ -15,13 +15,15 @@ var elevenLabsApiKeyConfig = config["ElevenLabsApiKey"];
 var elevenLabsVoiceIdConfig = config["VoiceId"];
 var ollamaUrlConfig = config["OllamaUrl"];
 var keepVoiceFilesConfig = config["KeepVoiceFiles"];
+var voiceFileDirectoryConfig = config["VoiceFileDirectory"];
 
 if (String.IsNullOrEmpty(modelConfig) ||
     String.IsNullOrEmpty(initialPromptConfig) ||
     String.IsNullOrEmpty(welcomeMessageConfig) ||
     String.IsNullOrEmpty(elevenLabsApiKeyConfig) ||
     String.IsNullOrEmpty(elevenLabsVoiceIdConfig) ||
-    String.IsNullOrEmpty(ollamaUrlConfig))
+    String.IsNullOrEmpty(ollamaUrlConfig) ||
+    String.IsNullOrEmpty(voiceFileDirectoryConfig))
 {
     Console.WriteLine("Local settings not found.");
     return;
@@ -33,6 +35,7 @@ var welcomeMessage = Environment.GetEnvironmentVariable(welcomeMessageConfig, En
 var elevenLabsApiKey = Environment.GetEnvironmentVariable(elevenLabsApiKeyConfig, EnvironmentVariableTarget.Machine);
 var elevenLabsVoiceId = Environment.GetEnvironmentVariable(elevenLabsVoiceIdConfig, EnvironmentVariableTarget.Machine);
 var ollamaUrl = Environment.GetEnvironmentVariable(ollamaUrlConfig, EnvironmentVariableTarget.Machine);
+var voiceFileDirectory = Environment.GetEnvironmentVariable(voiceFileDirectoryConfig, EnvironmentVariableTarget.Machine);
 
 var keepVoiceFiles = false;
 if (!String.IsNullOrEmpty(keepVoiceFilesConfig))
@@ -45,10 +48,21 @@ if (String.IsNullOrEmpty(model) ||
     String.IsNullOrEmpty(welcomeMessage) ||
     String.IsNullOrEmpty(elevenLabsApiKey) ||
     String.IsNullOrEmpty(elevenLabsVoiceId) ||
-    String.IsNullOrEmpty(ollamaUrl))
+    String.IsNullOrEmpty(ollamaUrl) ||
+    String.IsNullOrEmpty(voiceFileDirectory))
 {
     Console.WriteLine("Environment settings not found.");
     return;
+}
+
+if (!Path.Exists(voiceFileDirectory))
+{
+    Directory.CreateDirectory(voiceFileDirectory);
+}
+
+if (!Path.EndsInDirectorySeparator(voiceFileDirectory))
+{
+    voiceFileDirectory += Path.DirectorySeparatorChar;
 }
 
 var uri = new Uri(ollamaUrl);
@@ -77,15 +91,15 @@ while (!string.IsNullOrEmpty(line))
         var voice = await api.VoicesEndpoint.GetVoiceAsync(elevenLabsVoiceId);
         var defaultVoiceSettings = await api.VoicesEndpoint.GetDefaultVoiceSettingsAsync();
         var voiceClip = await api.TextToSpeechEndpoint.TextToSpeechAsync(text, voice, defaultVoiceSettings);
-        await File.WriteAllBytesAsync($"{voiceClip.Id}.mp3", voiceClip.ClipData.ToArray());
+        await File.WriteAllBytesAsync($"{voiceFileDirectory}{voiceClip.Id}.mp3", voiceClip.ClipData.ToArray());
         Console.WriteLine(completion.Response);
         Console.WriteLine();
         var player = new Player();
-        player.Play($"{voiceClip.Id}.mp3").Wait();
+        player.Play($"{voiceFileDirectory}{voiceClip.Id}.mp3").Wait();
 
         if (!keepVoiceFiles)
         {
-            File.Delete($"{voiceClip.Id}.mp3");
+            File.Delete($"{voiceFileDirectory}{voiceClip.Id}.mp3");
         }
     }
     else
